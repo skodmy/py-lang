6b74cde4c7ecdea59cc3e90d5fb2edee6c3e151b
Skochynskiy Dmytro <skodmy@gmail.com>
2017-07-17 14:09:15 +0300

    App class added

--- 3xt_framework/core.py
+++ 3xt_framework/core.py
@@ -170,3 +170,36 @@ def execute_all_for_all(xxxt_files: List[str]) -> list:
     return [
         execute_all(xxxt_files, available_interpreter) for available_interpreter in __SETTINGS['AVAILABLE_INTERPRETERS']
     ]
+
+
+class App:
+    """
+    Class that represents an 3xt app.
+    
+    directory - app's directory.
+    settings_filename - a name of a file with settings for the app.
+    __settings - a dictionary with settings for the app.
+    """
+    directory = __file__
+    settings_filename = 'settings.py'
+    __settings = dict()
+
+    def __init__(self, directory: str=None, settings_filename: str=None):
+        if directory is not None:
+            self.directory = directory
+        if settings_filename is not None:
+            self.settings_filename = settings_filename
+        populate_settings_with_file(self.settings_filename, self.directory)
+        self.__settings = settings()
+        self.__settings['XXXT_FILES'] = explore_for_files(self.directory)
+
+    def run(self) -> None:
+        """
+        Run the app.
+        
+        :return: None.
+        """
+        results = execute_all_for_all(self.__settings['XXXT_FILES'])
+        if self.__settings['DISPLAY_RESULT']:
+            for result in results:
+                print(result)
