cb86d66252fdac5c1cb30a0face63036eaf556ea
Skochynskiy Dmytro <skodmy@gmail.com>
2017-07-17 22:08:39 +0300

    moved is_available from core.py to utilities.py

--- xxxt_framework/core.py
+++ xxxt_framework/core.py
@@ -63,24 +63,6 @@ def populate_settings_with_file(filename: str= 'settings.py', directory: str= '.
                 set_setting(setting_name, getattr(settings_module, setting_name))
 
 
-def is_available(exec_name: str) -> bool:
-    """
-    Checks if an executable with a name exec_name is available. 
-    
-    :param exec_name: the name of the executable.
-    :return: True if it's available, False otherwise.
-    """
-    if exec_name == '' or exec_name is None:
-        raise ValueError('exec_name can not be None or empty string!')
-    try:
-        completed_process = __run((exec_name, '--version'), stdout=__PIPE, stderr=__PIPE)
-    except FileNotFoundError:
-        return False
-    if completed_process.returncode == 0:
-        return True
-    return False
-
-
 def list_available_interpreters(interpreters_exec_names: List[str] = None) -> List[str]:
     """
     Produces a list of available interpreters.
--- xxxt_framework/utilities.py
+++ xxxt_framework/utilities.py
@@ -1,3 +1,5 @@
+from subprocess import run, PIPE
+
 INTERPRETERS_EXECUTABLES_NAMES = (
         'python2',
         'python3',
@@ -5,3 +7,21 @@ INTERPRETERS_EXECUTABLES_NAMES = (
         'pypy3',
         'jython',
 )
+
+
+def is_available(exec_name: str) -> bool:
+    """
+    Checks if an executable with a name exec_name is available. 
+
+    :param exec_name: the name of the executable.
+    :return: True if it's available, False otherwise.
+    """
+    if exec_name == '' or exec_name is None:
+        raise ValueError('exec_name can not be None or empty string!')
+    try:
+        completed_process = run((exec_name, '--version'), stdout=PIPE, stderr=PIPE)
+    except FileNotFoundError:
+        return False
+    if completed_process.returncode == 0:
+        return True
+    return False
