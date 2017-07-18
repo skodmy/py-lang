c951161d31bb4a49ffb26356bec06091827ad5b2
Skochynskiy Dmytro <skodmy@gmail.com>
2017-07-18 16:25:47 +0300

    optional parameters added to execute_all_for_all, execute_xxxt_file, explore_for_files in core.py

--- xxxt_framework/core.py
+++ xxxt_framework/core.py
@@ -4,8 +4,8 @@ from typing import List, Any
 
 __SETTINGS = {
     'ENVIRONMENT': os.name,
-    'AVAILABLE_INTERPRETERS': ['python3'],
-    'XXXT_FILES_SUFFIXES': ['spd', 'mmr'],
+    'AVAILABLE_INTERPRETERS_EXECUTABLES_NAMES': ['python3'],
+    'XXXT_FILES_NAMES_SUFFIXES': ['spd', 'mmr'],
     'DISPLAY_RESULT': True,
     'WRITE_RESULT_TO_FILE': False,
     'RESULT_FILE_EXTENSION': 'txt',
@@ -63,44 +63,50 @@ def populate_settings_with_file(filename: str= 'settings.py', directory: str= '.
                 set_setting(setting_name, getattr(settings_module, setting_name))
 
 
-def list_files_names(interpreters_names: List[str]) -> List[str]:
+def list_files_names(interpreters_execs_names: List[str]) -> List[str]:
     """
     Produces a list of files names for results.
     
-    :param interpreters_names: 
+    :param interpreters_execs_names: 
     :return: 
     """
-    if interpreters_names is None or len(interpreters_names) == 0:
+    if interpreters_execs_names is None or len(interpreters_execs_names) == 0:
         raise ValueError
-    return [interpreter_name + '.txt' for interpreter_name in interpreters_names]
+    return [interpreter_name + '.txt' for interpreter_name in interpreters_execs_names]
 
 
-def explore_for_files(directory: str='.') -> List[str]:
+def explore_for_files(directory: str='.', files_names_suffixes: List[str]=None) -> List[str]:
     """
     Explores a directory for files that match filter's condition.
     
     :param directory: the directory which will be explored for files.
+    :param files_names_suffixes: a list of suffixes with which should end files in the directory.
     :return: a list of found files.
     """
+    if files_names_suffixes is None:
+        files_names_suffixes = __SETTINGS['XXXT_FILES_NAMES_SUFFIXES']
     return [
         entry for entry in os.listdir(directory)
         if any([
-            entry.endswith('_' + suffix + 't.py') for suffix in __SETTINGS['XXXT_FILES_SUFFIXES']
+            entry.endswith('_' + file_name_suffix + 't.py') for file_name_suffix in files_names_suffixes
         ])
     ]
 
 
-def execute_xxxt_file(filename: str, interpreter_exec_name: str) -> dict:
+def execute_xxxt_file(filename: str, interpreter_exec_name: str, files_names_suffixes: List[str]=None) -> dict:
     """
     Executes a xxxt file with a given interpreter's executable name.
     
     :param filename: a name of the xxxt file.
     :param interpreter_exec_name: interpreter's executable name.
+    :param files_names_suffixes: a list of suffixes with which should end file.
     :return: a dictionary with a result of execution.
     """
     if filename is None or filename == '':
         raise ValueError
-    for suffix in __SETTINGS['XXXT_FILES_SUFFIXES']:
+    if files_names_suffixes is None:
+        files_names_suffixes = __SETTINGS['XXXT_FILES_NAMES_SUFFIXES']
+    for suffix in files_names_suffixes:
         if filename.endswith('_' + suffix + 't.py'):
             break
     else:
@@ -128,13 +134,16 @@ def execute_all(xxxt_files: List[str], interpreter_exec_name: str) -> dict:
     return {xxxt_file: execute_xxxt_file(xxxt_file, interpreter_exec_name) for xxxt_file in xxxt_files}
 
 
-def execute_all_for_all(xxxt_files: List[str]) -> list:
+def execute_all_for_all(xxxt_files: List[str], interpreters_execs_names: List[str]=None) -> list:
     """
     Executes all xxxt files with names from a given list for all available interpreters.
     
     :param xxxt_files: a list with xxxt files names.
+    :param interpreters_execs_names: a list with interpreters executables names.
     :return: a list of dictionaries which describes a status of execution xxxt files for each available interpreter.
     """
+    if interpreters_execs_names is None:
+        interpreters_execs_names = __SETTINGS['AVAILABLE_INTERPRETERS_EXECUTABLES_NAMES']
     return [
-        execute_all(xxxt_files, available_interpreter) for available_interpreter in __SETTINGS['AVAILABLE_INTERPRETERS']
+        execute_all(xxxt_files, interpreter_exec_name) for interpreter_exec_name in interpreters_execs_names
     ]
